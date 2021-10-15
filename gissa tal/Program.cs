eccac53562b31d6d40fb27aa93ef42ffc87f34d4
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2021-10-15 15:43:05 +0200

    Add files via upload

---
+++ gissa tal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace gissa_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int slumpTal = slump.Next(100);
+            int antalGissningar = 0;
+            int gissning = 101;
+
+            while(gissning != slumpTal)
+            {
+                Console.WriteLine("Guess the randomly generated number!");
+                gissning = int.Parse(Console.ReadLine());
+                antalGissningar ++;
+
+                if(gissning > slumpTal){
+                    Console.WriteLine("Too high");
+                }
+                else if(gissning < slumpTal){
+                    Console.WriteLine("Too low");
+                }
+                else{
+                    Console.WriteLine("You guessed correctly! You answered in: "+antalGissningar+" attempts");
+                }
+            }
+        }
+    }
+}
